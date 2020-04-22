1a4b8a20723c5c8a74a61e8f24cf994a8ac4b3b1
wow-woo <fwpass@outlook.com>
2020-04-23 00:20:09 +0900

    toggling

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const nav = document.querySelector("#cont-nav");
 const btn_nav_hide = document.querySelector(".tit>.btn");
-btn_nav_hide.onClick = () => {
-  console.log(nav);
+console.log(nav);
+console.log(btn_nav_hide);
+
+let toggle = false;
+btn_nav_hide.onclick = function () {
+  if (toggle === true) {
+    nav.style.height = 0;
+    nav.style.opacity = "0";
+    nav.style.display = "none";
+    toggle = false;
+    console.log(toggle);
+
+    return;
+  }
+  nav.style.height = null;
+  nav.style.opacity = "1";
   nav.style.display = "block";
+
+  toggle = true;
+
+  console.log(toggle);
 };
